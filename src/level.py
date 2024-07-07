4bcd305c597f42fb79e4686c3bfcf48e5d8c587f
fortwoone <denauxarthur@gmail.com>
2024-07-07 19:37:23 +0200

    Keeping the branch up to date with main

--- src/level.py
+++ src/level.py
@@ -24,8 +24,9 @@ from src.settings import (
 
 class Level:
 
-    def __init__(self, tmx_maps: MapDict, character_frames, level_frames, overlay_frames, font, sounds, switch):
+    def __init__(self, game, tmx_maps: MapDict, character_frames, level_frames, overlay_frames, font, sounds, switch):
         self.display_surface = pygame.display.get_surface()
+        self.game = game
 
         # sprite groups
         self.entities = {}
@@ -135,7 +136,8 @@ class Level:
         # playable entities
         self.entities = {}
         for obj in tmx_maps['main'].get_layer_by_name('Entities'):
-            self.entities[obj.name] = Player(pos=(obj.x * SCALE_FACTOR, obj.y * SCALE_FACTOR),
+            self.entities[obj.name] = Player(game=self.game,
+                                             pos=(obj.x * SCALE_FACTOR, obj.y * SCALE_FACTOR),
                                              frames=character_frames['rabbit'],
                                              groups=self.all_sprites,
                                              collision_sprites=self.collision_sprites,
