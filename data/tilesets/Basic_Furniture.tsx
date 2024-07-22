76e850e99a0055dcf4ed67d79b1a3f9775090142
Sonja Ruth <donnie.darko1966@web.de>
2024-07-22 11:43:54 +0200

    add forest map and necessary tilesets and images

---
+++ data/tilesets/Basic_Furniture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Basic_Furniture" tilewidth="16" tileheight="16" tilecount="54" columns="9">
+ <image source="../../images/tilesets/Basic_Furniture.png" width="144" height="96"/>
+</tileset>
