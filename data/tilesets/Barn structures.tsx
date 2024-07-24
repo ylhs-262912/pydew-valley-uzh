a47b8a54fa7b64d0a9f7070f241ff3be7a588286
Sonja Ruth <donnie.darko1966@web.de>
2024-07-24 16:24:35 +0200

    add farm map and necessary files

---
+++ data/tilesets/Barn structures.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Barn structures" tilewidth="16" tileheight="16" tilecount="12" columns="3">
+ <image source="../../images/tilesets/Barn structures.png" width="48" height="64"/>
+</tileset>
