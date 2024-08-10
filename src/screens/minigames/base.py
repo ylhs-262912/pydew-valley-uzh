764b9118cc0859fda1c8da22356f0ef393200bf8
michelfinley <173732259+michelfinley@users.noreply.github.com>
2024-08-10 13:18:42 +0200

    refactor(minigames/base.py): remove abstractmethod decorator from partially implemented base methods

--- src/screens/minigames/base.py
+++ src/screens/minigames/base.py
@@ -22,7 +22,6 @@ class Minigame(ABC):
     __on_start_funcs: list[Callable[[], None]]
     __on_finish_funcs: list[Callable[[], None]]
 
-    @abstractmethod
     def __init__(self, state: MinigameStateType):
         self._state = state
 
@@ -39,7 +38,6 @@ class Minigame(ABC):
     def on_finish(self, func: Callable[[], None]):
         self.__on_finish_funcs.append(func)
 
-    @abstractmethod
     def start(self):
         self.running = True
         self._ctime = 0
@@ -47,7 +45,6 @@ class Minigame(ABC):
         for func in self.__on_start_funcs:
             func()
 
-    @abstractmethod
     def finish(self):
         self.running = False
 
@@ -58,7 +55,6 @@ class Minigame(ABC):
     def handle_event(self, event: pygame.Event):
         pass
 
-    @abstractmethod
     def update(self, dt: float):
         self._ctime += dt
 
