5cb7b8f3393ab1d39da72dfa7ed8d6d5b15f04fe
michelfinley <173732259+michelfinley@users.noreply.github.com>
2024-08-10 20:58:26 +0200

    refactor(minigames/base.py): make Minigame.running a readonly property

--- src/screens/minigames/base.py
+++ src/screens/minigames/base.py
@@ -25,12 +25,16 @@ class Minigame(ABC):
     def __init__(self, state: MinigameStateType):
         self._state = state
 
-        self.running = False
+        self._running = False
 
         self._ctime = 0
 
         self.__on_start_funcs = []
         self.__on_finish_funcs = []
+
+    @property
+    def running(self):
+        return self._running
 
     def on_start(self, func: Callable[[], None]):
         self.__on_start_funcs.append(func)
@@ -39,14 +43,14 @@ class Minigame(ABC):
         self.__on_finish_funcs.append(func)
 
     def start(self):
-        self.running = True
+        self._running = True
         self._ctime = 0
 
         for func in self.__on_start_funcs:
             func()
 
     def finish(self):
-        self.running = False
+        self._running = False
 
         for func in self.__on_finish_funcs:
             func()
