f1cbf4785ae2b9c055c4630e34cea4c18bd71418
Raphaël Pietrzak <raphael.pietrzak@etu.univ-amu.fr>
2024-08-15 18:48:23 +0200

    art: volcano spritesheets

--- src/groups.py
+++ src/groups.py
@@ -57,3 +57,4 @@ class AllSprites(PersistentSpriteGroup):
             for sprite in sorted_sprites:
                 if sprite.z == layer:
                     sprite.draw(self.display_surface, self.offset)
+
---
+++ src/gui/scene_anim.py
@@ -0,0 +1,34 @@
+import pygame
+
+
+class SceneAnimation:
+    def __init__(self, target_points):
+        self.target_points = target_points 
+        self.speed = 200
+        self.current_index = 0  
+        self.current_pos = pygame.Vector2(target_points[0])
+        self.is_finished = False
+
+    def update(self, dt):
+        if self.is_finished or self.current_index >= len(self.target_points) - 1:
+            return
+
+        target = pygame.Vector2(self.target_points[self.current_index])
+        direction = target - self.current_pos
+
+        if direction.length() <= self.speed * dt:
+            self.current_pos = target
+            self.current_index += 1
+            if self.current_index >= len(self.target_points):
+                self.is_finished = True
+        else:
+            direction = direction.normalize()
+            self.current_pos += direction * self.speed * dt
+
+    def get_current_position(self):
+        return self.current_pos
+
+    def reset(self):
+        self.current_index = 0
+        self.current_pos = pygame.Vector2(self.target_points[0])
+        self.is_finished = False
