034d2d69d03d3cc2f967458652180b81c6b1a136
Hubert Nafalski <hubert.nafalski@gmail.com>
2024-10-01 08:28:18 +0200

    * enabling cow and chicken wander around

--- src/npc/behaviour/chicken_behaviour_tree.py
+++ src/npc/behaviour/chicken_behaviour_tree.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum
 
+from pathfinding.core.grid import Grid
+
 from src.npc.bases.chicken_base import ChickenBase
 from src.npc.behaviour.ai_behaviour_tree_base import (
     Action,
@@ -16,10 +18,11 @@ from src.npc.utils import pf_wander
 @dataclass
 class ChickenIndividualContext(Context):
     chicken: ChickenBase
+    range_grid: Grid = None
 
 
 def wander(context: ChickenIndividualContext) -> bool:
-    return pf_wander(context.chicken)
+    return pf_wander(context.chicken, pf_grid=ChickenIndividualContext.range_grid)
 
 
 class ChickenBehaviourTree(NodeWrapper, Enum):
